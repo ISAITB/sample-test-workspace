389310729d17a683f4f39c7ca855d967d8da307f
simatosc <costas.simatos@sigmacubed.eu>
2025-06-10 14:17:48 +0200

    Updates for next release

--- deploy_test_suite.bat
+++ deploy_test_suite.bat
@@ -1,3 +1,3 @@
 del testSuite1.zip
 7z a testSuite1.zip .\testSuite1\*
-curl -F updateSpecification=true -F specification=4F4410E1X430DX4E1BXB064X0CECA6A3C0D4 -F testSuite=@testSuite1.zip --header "ITB_API_KEY: D355B62BXE40BX494FX8B0AXEA8E202CAD8A" -X POST http://localhost:9000/api/rest/testsuite/deploy
+curl -F updateSpecification=true -F showIdentifiers=false -F specification=4F4410E1X430DX4E1BXB064X0CECA6A3C0D4 -F testSuite=@testSuite1.zip --header "ITB_API_KEY: D355B62BXE40BX494FX8B0AXEA8E202CAD8A" -X POST http://localhost:9000/api/rest/testsuite/deploy
